b79e326763970996d2a4dfc06209e227d69fcfaa
Taweesak02 <taweesak.n@ku.th>
2025-09-02 20:52:34 +0700

    add validator input

--- lib/bmipage.dart
+++ lib/bmipage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'validators.dart';
 
 class BMIPage extends StatefulWidget {
   const BMIPage({super.key});
@@ -7,41 +8,83 @@ class BMIPage extends StatefulWidget {
 }
 
 class _BMIPageState extends State<BMIPage> {
+  //ช่องแสดงครั้งแรกเมื่อกดปุ่มครั้งแรก
   bool pressBtFirst = false;
+  //ตัวแปรส่วนสูงน้ำหนัก
+  double? height;
+  double? weight;
+
+  final _formKey = GlobalKey<FormState>();
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("BMI Calculator")),
+      appBar: AppBar(title: Text("คำนวณดัชนีมวลกาย")),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            spacing: 10,
-            children: [
-              SizedBox(
-                width: 200,
-                child: TextField(
-                  decoration: InputDecoration(labelText: "น้ำหนัก(กิโลกรัม)"),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              spacing: 10,
+              children: [
+                SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: "น้ำหนัก(กิโลกรัม)"),
+                    validator: Validators.multiValidator([
+                      Validators.required(errorMessage: "กรุณากรอกข้อมูล"),
+                      Validators.numberValidator(
+                        errorMessage: "กรุณากรอกตัวเลข",
+                      ),
+                      Validators.minValue(
+                        errorMessage: "เลขไม่ถูกต้อง",
+                        minValue: 1,
+                      ),
+                    ]),
+                    onChanged: (value) {
+                      weight = double.tryParse(value);
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 200,
-                child: TextField(
-                  decoration: InputDecoration(labelText: "ส่วนสูง(เซนติเมตร)"),
+                SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "ส่วนสูง(เซนติเมตร)",
+                    ),
+                    validator: Validators.multiValidator([
+                      Validators.required(errorMessage: "กรุณากรอกข้อมูล"),
+                      Validators.numberValidator(
+                        errorMessage: "กรุณากรอกตัวเลข",
+                      ),
+                      Validators.minValue(
+                        errorMessage: "เลขไม่ถูกต้อง",
+                        minValue: 1,
+                      ),
+                    ]),
+                    onChanged: (value) {
+                      height = double.tryParse(value);
+                    },
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      pressBtFirst = true;
-                    });
-                  },
-                  child: Text("คำนวณ"),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (!_formKey.currentState!.validate()) {
+                          pressBtFirst = false;
+                          return;
+                        }
+                        pressBtFirst = true;
+                      });
+                    },
+                    child: Text("คำนวณ"),
+                  ),
                 ),
-              ),
-              if (pressBtFirst) showBlock(),
-            ],
+                if (pressBtFirst) showBlock(weight!, height!),
+              ],
+            ),
           ),
         ),
       ),
@@ -49,14 +92,21 @@ class _BMIPageState extends State<BMIPage> {
   }
 }
 
-Widget showBlock() {
+Widget showBlock(double weight, double height) {
   return Padding(
     padding: const EdgeInsets.all(20),
     child: Container(
-      decoration: BoxDecoration(color: Colors.grey[200],borderRadius: BorderRadius.circular(20)),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(20),
+      ),
       width: 500,
       height: 500,
-      child: Column(),
+      child: Column(
+        children: [
+          Text((weight + height).toString())
+        ],
+      ),
     ),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,35 @@ import 'bmipage.dart';
 import 'page2.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: FirstRoute(), // หรือ widget หลักของคุณ
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstRoute(), // หรือ widget หลักของคุณ
+    ),
+  );
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key, required this.title, this.image = '',required this.page});
+
+  //รูปภาพพื้นหลังของปุ่ม !! ยังคิดไม่ออก
+  final String image;
+  //หัวข้อของปุ่ม
+  final String title;
+  //หน้าที่ต้องการไป
+  final Widget page;
+
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(title),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+    );
+  }
 }
 
 class FirstRoute extends StatelessWidget {
@@ -15,31 +40,16 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('First Route')),
-      body: Column(
-        children:[ 
-          ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const Page2()),
-              );
-            },
-          ),
-          ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const BMIPage()),
-            );
-          },)
-        
-
-        ]
+      appBar: AppBar(title: const Text('รักสุขภาพ')),
+      body: Center(
+        child: Column(
+          spacing: 20,
+          children: [
+            ButtonApp(title: "คำนวณดัชนีมวลกาย", page: const BMIPage()),
+            ButtonApp(title: "ผักและการออกกำลังกาย", page: const Page2()),
+            
+          ],
+        ),
       ),
     );
   }
